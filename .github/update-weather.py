47e30e1fb40f0571ac0b420691a8a69800191fa4
Leo <5376265+leoherzog@users.noreply.github.com>
2022-05-20 15:10:07 +0000

    Switch to Freedom Units

--- .github/update-weather.py
+++ .github/update-weather.py
@@ -4,12 +4,12 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-weather = requests.get('https://swd.weatherflow.com/swd/rest/better_forecast?station_id=71686&token=' + os.environ['TEMPEST_PUT']).json()
+weather = requests.get('https://swd.weatherflow.com/swd/rest/better_forecast?station_id=71686&token=' + os.environ['TEMPEST_PUT'] + '&units_temp=f').json()
 
 print(weather)
 
 profile = BeautifulSoup(open('README.md'), 'html.parser')
-profile.find(id='weather').string = str(round(weather['current_conditions']['air_temperature'], 1)) + '°' + weather['units']['units_temp'] + ' and ' + weather['current_conditions']['conditions']
+profile.find(id='weather').string = str(round(weather['current_conditions']['air_temperature'], 1)) + '°' + weather['units']['units_temp'].upper() + ' and ' + weather['current_conditions']['conditions']
 
 overwrite = open('README.md', 'w')
 overwrite.write(profile.prettify())
