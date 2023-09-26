1792c84d0925a053a41261caed5d7e960673935e
Leo Herzog <xd1936@gmail.com>
2023-09-26 12:16:42 -0400

    Reset

---
+++ .github/update-weather.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import urllib.request
+
+icons = {
+  "clear-day": "☀️",
+  "clear-night": "🌙",
+  "cloudy": "☁️",
+  "foggy": "🌁",
+  "partly-cloudy-day": "⛅",
+  "partly-cloudy-night": "⛅",
+  "possibly-rainy-day": "🌦️",
+  "possibly-rainy-night": "🌦️",
+  "possibly-sleet-day": "🌨️",
+  "possibly-sleet-night": "🌨️",
+  "possibly-snow-day": "🌨️",
+  "possibly-snow-night": "🌨️",
+  "possibly-thunderstorm-day": "⛈️",
+  "possibly-thunderstorm-night": "⛈️",
+  "rainy": "🌧️",
+  "sleet": "🌨️",
+  "snow": "🌨️",
+  "thunderstorm": "⛈️",
+  "windy": "🍃"
+}
+
+url = 'https://swd.weatherflow.com/swd/rest/better_forecast?station_id=71686&token=' + os.environ['TEMPEST_PUT'] + '&units_temp=f'
+with urllib.request.urlopen(url) as response:
+  data = response.read()
+  weather = json.loads(data.decode('utf-8'))
+
+print(weather)
+
+profile = open('README.md', 'r', encoding='utf-8').read()
+start_pos = profile.find('\u200B') + len('\u200B') # zero-width spaces
+end_pos = profile.find('\u200B', start_pos)
+
+temp = str(round(weather['current_conditions']['air_temperature'])) + '°' + weather['units']['units_temp'].upper()
+condition = icons[weather['current_conditions']['icon']] + ' ' + weather['current_conditions']['conditions']
+profile = profile[:start_pos] + temp + ' and ' + condition + profile[end_pos:]
+
+overwrite = open('README.md', 'w', encoding='utf-8')
+overwrite.write(profile)
+overwrite.close()
---
+++ README.md
@@ -0,0 +1,17 @@
+# 👋 Hello! I'm Leo.
+
+I'm a technology enthusiast living in West Michigan, where it's ​69°F and ⛅ Partly Cloudy​ right now. I currently work at [Hope College](https://hope.edu/directory/people/herzog-leo/) and have enthusiasm for too many things.
+
+I enjoy working with:
+- Javascript and Google Apps Script
+- Python
+- Linux and Raspberry Pis
+- ESP32/ESP8266 and Arduino
+- Decentralization and the Distributed Web
+- Podcast Tools
+- Social Media APIs
+- Hardware Diagnosis and Repair
+- Weather Data
+- Ham Radio
+
+I'm a learner, teacher, tea enthustast, campfire ukulele player, flag enthusiast, weather spotter, [podcaster](https://spitball.show/), comedy lover, and your friend.
